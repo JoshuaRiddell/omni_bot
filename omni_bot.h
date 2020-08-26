5aaef30e891ab8e6354405b4ff0e5da79883fd99
Joshua Riddell <jridd30@gmail.com>
2020-08-27 07:44:25 +1000

    Update to new kinematics version

--- omni_bot.h
+++ omni_bot.h
@@ -1,7 +1,17 @@
 #include "stm32f4xx_hal.h"
 
-void stepper_setup();
+typedef struct {
+	double x;
+	double y;
+	double theta;
+} position_t;
+
+void pin_setup(void);
+void timer_setup(void);
 void motion_setup();
 void enable_steppers();
+void kinematics_setup();
 void disable_steppers();
 void move_robot(float x, float y, float w);
+float get_speed(uint8_t motor_num);
+position_t get_position();
